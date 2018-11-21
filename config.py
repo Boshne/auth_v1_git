977e870d4314def2c6b0648f26973b98439b2cd0
boshenzhang <18637772876@163.com>
2018-11-21 11:32:25 +0800

    edit os.environ

--- config.py
+++ config.py
@@ -11,10 +11,12 @@ class Config:
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.163.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '25'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
-    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_USERNAME = '18637772876@163.com'
-    MAIL_PASSWORD = 'z326652929z'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    if not (MAIL_USERNAME and MAIL_PASSWORD):
+        raise ValueError("MAIL USERNAME OR PASSWORD HAS NOT BEEN WRITEN TO ENV VARIABLES.")
+    # MAIL_USERNAME = '18637772876@163.com'
+    # MAIL_PASSWORD = 'z326652929z'
     FLASKY_MAIL_SUBJECT_PREFIX = '[Customer Confirmation]'
     FLASKY_MAIL_SENDER = 'Fangling Admin <18637772876@163.com>'
     # FLASKY_ADMIN = os.environ.get('Fangling_Admin')
