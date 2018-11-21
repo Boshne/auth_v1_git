653e9d8904f7dc51d796b739d69a87f7aa1500c3
boshenzhang <18637772876@163.com>
2018-11-21 16:58:19 +0800

    add forms to generate key

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, Regexp
 
 
 class NameForm(FlaskForm):
-    name = StringField('What is your company\'s name?', validators=[DataRequired()])
+    name = StringField('What is your company\'s name?', validators=[
+        DataRequired()])
     submit = SubmitField('Submit')
+
+
+class GenerateKeyFrom(FlaskForm):
+    hasp_id = StringField('Hasp ID', validators=[
+        DataRequired(), Length(1,64), Regexp(
+            '^[0-9]*$', 0, 'must have only numbers')])
+    musk = StringField('Musk', validators=[
+        DataRequired(), Length(1, 64), Regexp(
+            '^[0-9]*$', 0, 'must have only numbers')])
+    password = StringField('Password', validators=[
+        DataRequired(), Length(1, 64), Regexp(
+            '^[0-9]*$', 0, 'must have only numbers')])
+    clays = StringField('Days', validators=[
+        DataRequired(), Length(1, 64), Regexp(
+            '^[0-9]*$', 0, 'must have only numbers')])
+    submit = SubmitField('Generate')
--- app/main/views.py
+++ app/main/views.py
@@ -5,14 +5,25 @@ Blueprint main
 from flask import render_template
 from . import main
 from ..models import User
+from flask_login import login_user, logout_user, login_required, current_user
+from .forms import GenerateKeyFrom
 
 
 @main.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
 
-@main.route('/user/<username>')
+@main.route('/user/<username>', methods=['GET', 'POST'])
+@login_required
 def user(username):
-    user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
+    form = GenerateKeyFrom()
+    if form.validate_on_submit():
+        hasp_id = form.hasp_id.data
+        musk = form.musk.data
+        password = form.password.data
+        clays = form.clays.data
+        data = hasp_id + musk + password + clays
+        data = data.encode('utf8')
+        return render_template('user.html', user=user, form=form, data=data)
+    return render_template('user.html', user=user, form=form)
